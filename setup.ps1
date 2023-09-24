43983a630c7e6543e89e259fa844f2f2a556fbae
Lord225 <zlotymaciej@gmail.com>
2023-09-24 15:46:20 +0200

    cleanup

--- setup.ps1
+++ setup.ps1
@@ -8,38 +8,35 @@ $root = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 # check if # lords-asm-for-mc-begin comment exists in $profile, if so remove everything between # lords-asm-for-mc-begin and # lords-asm-for-mc-end
 if (Select-String -Path $PROFILE -Pattern "lords-asm-for-mc-begin") {
-    Write-Host "Removing previous installation of 'lords-asm-for-mc' from $PROFILE."
+    Write-Host "Removing previous installation of 'lords-asm-for-mc' from $PROFILE"
     $content = Get-Content $PROFILE -Raw
     $content = $content -replace "(?ms)(\# lords-asm-for-mc-begin)(.*?)(\# lords-asm-for-mc-end)\s*", ""
     Set-Content $PROFILE $content
 }
 
-Write-Host "Installing dependencies for 'lords-asm-for-mc' in $root using pip."
 # install requirements
+Write-Host "Installing dependencies for 'lords-asm-for-mc' in $root using pip"
 pip install -r requirements.txt
 
-# add # lords-asm-for-mc-begin comment to $profile.CurrentUserAllHosts (with a newline)
+# add # lords-asm-for-mc-begin comment to $profile
 "# lords-asm-for-mc-begin" | Out-File -FilePath $PROFILE -Append
+"# do not edit, remove or change these comments, it is used by 'lords-asm-for-mc' to install and uninstall itself" | Out-File -FilePath $PROFILE -Append
 
-# Define the alias Name: lor, Value: python $compile, Description: Lord's ASM Compiler, add to $profile
+# add alias to compile.py
 $compile = Join-Path $root "compile.py"
-
-# funcytion lor that calls python $compile with arguments, write to $profile
 "function lor { python `"$compile`" @args }" | Out-File -FilePath $PROFILE -Append
 
-
+# add alias to send.py
 $send = Join-Path $root "tools" "send.py"
 "function rfsend { python `"$send`" @args }" | Out-File -FilePath $PROFILE -Append
 
-# Inform the user
-Write-Host "Alias 'lor' created for 'python $compile' and added to $PROFILE."
+Write-Host "Alias 'lor' and 'rfsend' created in $PROFILE"
 
 # add $root to the PATH, if not already there
-
-Write-Host "Adding '$root' to the PATH environment variable."
 "`$env:PATH  += `";$root`"" | Out-File -FilePath $PROFILE -Append
-
+Write-Host "Added '$root' to the PATH"
 
 "# lords-asm-for-mc-end" | Out-File -FilePath $PROFILE -Append
 
-Write-Host "You may need to restart your PowerShell session for changes to take effect."
+Write-Host "Done"
+Write-Host "You may need to restart your PowerShell session for changes to take effect"
